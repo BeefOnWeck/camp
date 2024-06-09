25cae8be07d60b06ecafdf41dc0c7325b197812c
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2024-06-09 15:54:38 -0400

    Adding a bounding box to each scenery entity.

--- src/scenery.rs
+++ src/scenery.rs
@@ -1,5 +1,6 @@
 use crate::loading::TextureAssets;
 use crate::GameState;
+use bevy::math::bounding::Aabb2d;
 use bevy::prelude::*;
 
 pub struct SceneryPlugin;
@@ -9,7 +10,77 @@ pub struct Scenery;
 
 impl Plugin for SceneryPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::Playing), spawn_scenery);
+        app.add_systems(OnEnter(GameState::Playing), (
+            spawn_scenery,
+            // compute_bounding_boxes.after(spawn_scenery)
+        ));
+    }
+}
+
+#[derive(Component)]
+struct BoundingBox {
+    aabb: Aabb2d
+}
+
+#[derive(Bundle)]
+struct BoundedSprite {
+    sprite: SpriteBundle,
+    tile: ImageScaleMode,
+    bounds: BoundingBox
+}
+
+impl BoundedSprite {
+    fn from_texture(texture: Handle<Image>, trans_x: f32, trans_y: f32, trans_z: f32) -> Self {
+        BoundedSprite {
+            sprite: SpriteBundle {
+                texture,
+                transform: Transform::from_translation(Vec3::new(trans_x, trans_y, trans_z)),
+                ..Default::default()
+            },
+            tile: ImageScaleMode::Tiled {
+                tile_x: false,
+                tile_y: false,
+                stretch_value: 1.0,
+            },
+            bounds: BoundingBox {
+                aabb: Aabb2d {
+                    min: Vec2::ZERO,
+                    max: Vec2::ZERO
+                }
+            }
+        }
+    }
+
+    fn tiled_from_texture(
+        texture: Handle<Image>, 
+        trans_x: f32, trans_y: f32, trans_z: f32,
+        tile_x: bool, tile_y: bool, stretch_factor: f32
+    ) -> Self {
+        BoundedSprite {
+            sprite: SpriteBundle {
+                texture,
+                transform: Transform {
+                    translation: Vec3::new(trans_x, trans_y, trans_z),
+                    rotation: Quat::IDENTITY,
+                    scale: Vec3::new(
+                        if tile_x {stretch_factor} else {1.0}, 
+                        if tile_y {stretch_factor} else {1.0}, 
+                        1.0)
+                },
+                ..Default::default()
+            },
+            tile: ImageScaleMode::Tiled {
+                tile_x,
+                tile_y,
+                stretch_value: 1.0 / stretch_factor,
+            },
+            bounds: BoundingBox {
+                aabb: Aabb2d {
+                    min: Vec2::ZERO,
+                    max: Vec2::ZERO
+                }
+            }
+        }
     }
 }
 
@@ -27,47 +98,32 @@ fn spawn_scenery(mut commands: Commands, textures: Res<TextureAssets>) {
         })
         .with_children(|parent| {
             // Trees
-            parent.spawn((
-                SpriteBundle {
-                    texture: textures.tree1.clone(),
-                    transform: Transform {
-                        translation: Vec3::new(440.0 / 0.5, 0.0, 0.0),
-                        rotation: Quat::IDENTITY,
-                        scale: Vec3::new(1.0, 15.0, 1.0),
-                    },
-                    ..Default::default()
-                },
-                ImageScaleMode::Tiled {
-                    tile_x: false,
-                    tile_y: true,
-                    stretch_value: 1.0 / 15.0,
-                },
+            parent.spawn(BoundedSprite::tiled_from_texture(
+                textures.tree1.clone(), 
+                440.0 / 0.5, 0.0, 0.0,
+                false, true, 15.0
             ));
             // Cars
-            parent.spawn(SpriteBundle {
-                texture: textures.car_right_gray.clone(),
-                transform: Transform::from_translation(Vec3::new(340.0 / 0.5, -600.0 / 0.5, 0.0)),
-                ..Default::default()
-            });
-            parent.spawn(SpriteBundle {
-                texture: textures.car_right_blue.clone(),
-                transform: Transform::from_translation(Vec3::new(340.0 / 0.5, -500.0 / 0.5, 0.0)),
-                ..Default::default()
-            });
-            parent.spawn(SpriteBundle {
-                texture: textures.car_right_red.clone(),
-                transform: Transform::from_translation(Vec3::new(340.0 / 0.5, -400.0 / 0.5, 0.0)),
-                ..Default::default()
-            });
+            parent.spawn(BoundedSprite::from_texture(
+                textures.car_right_gray.clone(),
+                340.0 / 0.5, -600.0 / 0.5, 0.0
+            ));
+            parent.spawn(BoundedSprite::from_texture(
+                textures.car_right_blue.clone(),
+                340.0 / 0.5, -500.0 / 0.5, 0.0
+            ));
+            parent.spawn(BoundedSprite::from_texture(
+                textures.car_right_red.clone(),
+                340.0 / 0.5, -400.0 / 0.5, 0.0
+            ));
             // House
-            parent.spawn(SpriteBundle {
-                texture: textures.house.clone(),
-                transform: Transform::from_translation(Vec3::new(0.0, -200.0 / 0.5, 0.0)),
-                ..Default::default()
-            });
+            parent.spawn(BoundedSprite::from_texture(
+                textures.house.clone(),
+                0.0, -200.0 / 0.5, 0.0
+            ));
             // Fence horizontal
-            parent.spawn((
-                SpriteBundle {
+            parent.spawn(BoundedSprite {
+                sprite: SpriteBundle {
                     texture: textures.fence_horizontal.clone(),
                     transform: Transform {
                         translation: Vec3::new(-280.0 / 0.5, -300.0 / 0.5, -0.1),
@@ -76,42 +132,50 @@ fn spawn_scenery(mut commands: Commands, textures: Res<TextureAssets>) {
                     },
                     ..Default::default()
                 },
-                ImageScaleMode::Tiled {
+                tile: ImageScaleMode::Tiled {
                     tile_x: true,
                     tile_y: false,
                     stretch_value: 1.0 / 15.0,
                 },
-            ));
-            // Fence corner
-            parent.spawn(SpriteBundle {
-                texture: textures.fence_left_corner.clone(),
-                transform: Transform {
-                    translation: Vec3::new(-472.0 / 0.5, -300.0 / 0.5, -0.1),
-                    rotation: Quat::IDENTITY,
-                    scale: Vec3::new(1.0, 2.0, 1.0),
-                },
-                ..Default::default()
+                bounds: BoundingBox {
+                    aabb: Aabb2d {
+                        min: Vec2::ZERO,
+                        max: Vec2::ZERO
+                    }
+                }
             });
-            // Fence vertical
-            parent.spawn((
-                SpriteBundle {
-                    texture: textures.fence_vertical.clone(),
+            // Fence corner
+            parent.spawn(BoundedSprite {
+                sprite: SpriteBundle {
+                    texture: textures.fence_left_corner.clone(),
                     transform: Transform {
-                        translation: Vec3::new(-483.0 / 0.5, 0.0 / 0.5, -0.2),
+                        translation: Vec3::new(-472.0 / 0.5, -300.0 / 0.5, -0.1),
                         rotation: Quat::IDENTITY,
-                        scale: Vec3::new(1.0, 60.0, 1.0),
+                        scale: Vec3::new(1.0, 2.0, 1.0),
                     },
                     ..Default::default()
                 },
-                ImageScaleMode::Tiled {
+                tile: ImageScaleMode::Tiled {
                     tile_x: false,
-                    tile_y: true,
-                    stretch_value: 1.0 / 60.0,
+                    tile_y: false,
+                    stretch_value: 1.0,
                 },
+                bounds: BoundingBox {
+                    aabb: Aabb2d {
+                        min: Vec2::ZERO,
+                        max: Vec2::ZERO
+                    }
+                }
+            });
+            // Fence vertical
+            parent.spawn(BoundedSprite::tiled_from_texture(
+                textures.fence_vertical.clone(), 
+                -483.0 / 0.5, 0.0 / 0.5, -0.2,
+                false, true, 60.0
             ));
             // Back fence
-            parent.spawn((
-                SpriteBundle {
+            parent.spawn(BoundedSprite {
+                sprite: SpriteBundle {
                     texture: textures.fence_horizontal.clone(),
                     transform: Transform {
                         translation: Vec3::new(0.0 / 0.5, 730.0 / 0.5, -0.1),
@@ -120,15 +184,21 @@ fn spawn_scenery(mut commands: Commands, textures: Res<TextureAssets>) {
                     },
                     ..Default::default()
                 },
-                ImageScaleMode::Tiled {
+                tile: ImageScaleMode::Tiled {
                     tile_x: true,
                     tile_y: false,
                     stretch_value: 1.0 / 40.0,
                 },
-            ));
+                bounds: BoundingBox {
+                    aabb: Aabb2d {
+                        min: Vec2::ZERO,
+                        max: Vec2::ZERO
+                    }
+                }
+            });
             // Front fence (invisible and just a barrier)
-            parent.spawn((
-                SpriteBundle {
+            parent.spawn(BoundedSprite {
+                sprite: SpriteBundle {
                     texture: textures.fence_horizontal.clone(),
                     transform: Transform {
                         translation: Vec3::new(0.0 / 0.5, -730.0 / 0.5, -2.0),
@@ -137,12 +207,18 @@ fn spawn_scenery(mut commands: Commands, textures: Res<TextureAssets>) {
                     },
                     ..Default::default()
                 },
-                ImageScaleMode::Tiled {
+                tile: ImageScaleMode::Tiled {
                     tile_x: true,
                     tile_y: false,
                     stretch_value: 1.0 / 40.0,
                 },
-            ));
+                bounds: BoundingBox {
+                    aabb: Aabb2d {
+                        min: Vec2::ZERO,
+                        max: Vec2::ZERO
+                    }
+                }
+            });
         })
         .insert(Scenery);
 }
